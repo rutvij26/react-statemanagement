8328c14fbb3a009e208bb5944ecad0035f0a5e16
Rutvij Sathe <rut26sathe@gmail.com>
2021-01-27 11:45:00 +0530

    first

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { MovieContext } from "./MovieContext";
+
+const Nav = () => {
+  const [movies, setMovies] = useContext(MovieContext);
+
+  return (
+    <nav>
+      <h3>Rutvij</h3>
+      <p>List of Movies: {movies.length} </p>
+    </nav>
+  );
+};
+
+export default Nav;
